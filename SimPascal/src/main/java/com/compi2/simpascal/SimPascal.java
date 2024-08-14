2c615f3babfddee751a24a31cb64fd2afd711904
Monserrat Gomez <mariagomez202030849@cunoc.edu.gt>
2024-08-13 22:25:21 -0600

    Initial Commit

---
+++ SimPascal/src/main/java/com/compi2/simpascal/SimPascal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.compi2.simpascal;
+
+/**
+ *
+ * @author mgome
+ */
+public class SimPascal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
